bceef891b39a18ec7bd2ac5dc5650d5ea76d7929
codeyangjunwh <codeyangjunwh@gmail.com>
2025-01-03 21:32:38 +0800

    finished modules

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -7,7 +7,7 @@
 
 
 fn main() {
-    let cat = ("Furry McFurson", 3.5);
+    let cat = ("Furry McFurson", 23);
     let /* your pattern here */(name, age) = cat;
 
     println!("{} is {} years old.", name, age);
